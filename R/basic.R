f68b7aefbffa3303eada44892fbc7d1163e969b4
Danielle Navarro <d.navarro@unsw.edu.au>
2019-06-30 11:32:24 +1000

    use "job" in place of "project" to avoid name clashes

--- R/basic.R
+++ R/basic.R
@@ -1,32 +1,32 @@
 # the directory where project records should be stored
-project_home <- function() {
+job_home <- function() {
   getOption("projectr.home")
 }
 
-project_file <- function() {
-  file.path(project_home(), "projectr.json")
+job_file <- function() {
+  file.path(job_home(), "projectr.json")
 }
 
 # read project data from JSON file if it exists
-project_read <- function() {
-  if(file.exists(project_file())) {
-    return(jsonlite::fromJSON(project_file()))
+job_read <- function() {
+  if(file.exists(job_file())) {
+    return(jsonlite::fromJSON(job_file()))
   }
   return(list())
 }
 
 # write project data to JSON file
-project_write <- function(prj) {
-  prj_str <- jsonlite::toJSON(prj, pretty = TRUE)
-  writeLines(prj_str, project_file())
+job_write <- function(prj) {
+  job_str <- jsonlite::toJSON(prj, pretty = TRUE)
+  writeLines(job_str, job_file())
 }
 
 #' Create a new project
 #'
 #' @param name name of the project to create
 #' @export
-project_create <- function(name) {
-  prj <- project_read()
+job_create <- function(name) {
+  prj <- job_read()
   prj[[name]] <- list(name = name)
-  project_write(prj)
+  job_write(prj)
 }

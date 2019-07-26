e41b0d46bcf7597580616cc4eeedc905d682088d
Danielle Navarro <d.navarro@unsw.edu.au>
2019-07-26 13:44:01 +1000

    delete_job now confirms with user before deleting. closes #12

--- R/delete.R
+++ R/delete.R
@@ -14,8 +14,33 @@
 #' }
 delete_job <- function(jobname) {
   jobs <- job_read()
-  jobs[[jobname]] <- NULL
-  job_write(jobs)
+
+  # throw error if the job doesn't exist
+  if(is.null(jobs[[jobname]])) {
+    stop("Job '", jobname, "' does not exist", call. = FALSE)
+  }
+
+  # make the user confurm deleting
+  acc <- ""
+  while(acc != "y" & acc != "n") {
+    acc <- readline(paste0("Delete the workbch entry for job '", jobname , "'? [y/n] "))
+    acc <- tolower(acc)
+    acc <- trimws(acc, "both")
+  }
+
+  # delete if need be
+  if(acc == "y") {
+    jobs[[jobname]] <- NULL
+    job_write(jobs)
+    message("Job '", jobname, "' deleted")
+    return(invisible(NULL))
+  }
+
+  # tell the user it was aborted
+  if(acc == "n") {
+    message("Aborted. Nothing deleted.")
+    return(invisible(NULL))
+  }
 }
 
 

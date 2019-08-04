2d9aa857a9d7d80714758bb54dca822ce20e25ee
Danielle Navarro <d.navarro@unsw.edu.au>
2019-08-04 22:30:02 +1000

    my convenience function

--- misc/myjobs.R
+++ misc/myjobs.R
@@ -1,11 +1,20 @@
-set_tag(c("ambientnoise", "asciify", "attituderisk", "brownianbridge",
-  "ccsclass", "chdss", "chdss2018", "cogsciclass", "compcogscisydney",
-  "conjunction", "contextbandit", "curiousbandit", "deception",
-  "djnavarroblog", "echochambers", "evidence-selection", "expex",
-  "ggplot", "githubpages", "hugoslum", "itsraining", "kidbandit",
-  "lsrbook", "mindsmachines", "mipsmops", "multimodal", "mymemes",
-  "oddsandends", "ozunconf", "projects", "psyc2071", "psyr", "rainbowr",
-  "rladiessydneyblog", "satrdayjoburg", "skilledbandits", "skyliner",
-  "slumdown", "sydneyopenresearch", "thegitplace", "tidylsr", "tidylsrbook",
-  "typestokens", "uncertprot", "unswpsychwomen", "whalen", "workbch",
-  "xprmntr"), )
+# set_tag(c("ambientnoise", "asciify", "attituderisk", "brownianbridge",
+#   "ccsclass", "chdss", "chdss2018", "cogsciclass", "compcogscisydney",
+#   "conjunction", "contextbandit", "curiousbandit", "deception",
+#   "djnavarroblog", "echochambers", "evidence-selection", "expex",
+#   "ggplot", "githubpages", "hugoslum", "itsraining", "kidbandit",
+#   "lsrbook", "mindsmachines", "mipsmops", "multimodal", "mymemes",
+#   "oddsandends", "ozunconf", "projects", "psyc2071", "psyr", "rainbowr",
+#   "rladiessydneyblog", "satrdayjoburg", "skilledbandits", "skyliner",
+#   "slumdown", "sydneyopenresearch", "thegitplace", "tidylsr", "tidylsrbook",
+#   "typestokens", "uncertprot", "unswpsychwomen", "whalen", "workbch",
+#   "xprmntr"), )
+
+# j <- view_jobs(show_hidden = TRUE)
+# nj <- nrow(j)
+# for(i in 1:nj) {
+#   if(j$priority[i] > 3) {
+#     set_job_status(j$jobname[i], "masked")
+#   }
+# }
+#

be911b7bb8821d2be8c1581c5bd83786f89af0c4
Danielle Navarro <d.navarro@unsw.edu.au>
2019-08-04 15:13:34 +1000

    helper function for me

---
+++ misc/myjobs.R
@@ -0,0 +1,11 @@
+set_tag(c("ambientnoise", "asciify", "attituderisk", "brownianbridge",
+  "ccsclass", "chdss", "chdss2018", "cogsciclass", "compcogscisydney",
+  "conjunction", "contextbandit", "curiousbandit", "deception",
+  "djnavarroblog", "echochambers", "evidence-selection", "expex",
+  "ggplot", "githubpages", "hugoslum", "itsraining", "kidbandit",
+  "lsrbook", "mindsmachines", "mipsmops", "multimodal", "mymemes",
+  "oddsandends", "ozunconf", "projects", "psyc2071", "psyr", "rainbowr",
+  "rladiessydneyblog", "satrdayjoburg", "skilledbandits", "skyliner",
+  "slumdown", "sydneyopenresearch", "thegitplace", "tidylsr", "tidylsrbook",
+  "typestokens", "uncertprot", "unswpsychwomen", "whalen", "workbch",
+  "xprmntr"), )

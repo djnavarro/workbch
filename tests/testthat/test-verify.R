ec5e326f6363f360f6f1918c45b0f5d0ad74887b
Danielle Navarro <d.navarro@unsw.edu.au>
2019-08-09 16:46:05 +1000

    adds tests for verify_character and verify_status

--- tests/testthat/test-verify.R
+++ tests/testthat/test-verify.R
@@ -1,6 +1,9 @@
 test_that("verify_onestring works", {
   msg <- "must be character and length 1"
+
+  # should fail
   expect_error(verify_onestring(1), msg)
+  expect_error(verify_onestring(character(0L)), msg)
   expect_error(verify_onestring(NA), msg)
   expect_error(verify_onestring(NULL), msg)
   expect_error(verify_onestring(c("a", "b")), msg)
@@ -8,4 +11,53 @@ test_that("verify_onestring works", {
   expect_error(verify_onestring(matrix("a")), msg)
   expect_error(verify_onestring(matrix(character(0L))), msg)
   expect_error(verify_onestring(TRUE), msg)
+
+  # should succeed
+  expect_true(verify_onestring("asdf ^asf"))
+})
+
+test_that("verify_character works", {
+  msg <- "must be character"
+
+  # should fail
+  expect_error(verify_character(1), msg)
+  expect_error(verify_character(NA), msg)
+  expect_error(verify_character(NULL), msg)
+  expect_error(verify_character(list("a")), msg)
+  expect_error(verify_character(matrix("a")), msg)
+  expect_error(verify_character(matrix(character(0L))), msg)
+  expect_error(verify_character(matrix(c("a","b"))), msg)
+  expect_error(verify_character(TRUE), msg)
+
+  # should succeed
+  expect_true(verify_character(character(0L)))
+  expect_true(verify_character(NA_character_))
+  expect_true(verify_character("aasdfasdf"))
+  expect_true(verify_character(c("asdfasdf", "a")))
+})
+
+
+test_that("verify_status works", {
+
+  # cursory check that verify_onestring is called
+  msg <- "must be character and length 1"
+  expect_error(verify_status(1), msg)
+  expect_error(verify_status(c("active", "inactive")), msg)
+
+  # check that invalid status string are not accepted
+  msg <- "must be 'active', 'inactive'"
+  expect_error(verify_status("blarhg"), msg)
+  expect_error(verify_status("nactive"), msg)
+  expect_error(verify_status("Active"), msg)
+  expect_error(verify_status(" active"), msg)
+  expect_error(verify_status("active "), msg)
+  expect_error(verify_status("ac tive"), msg)
+
+  # should succeed
+  expect_true(verify_status("active"))
+  expect_true(verify_status("inactive"))
+  expect_true(verify_status("complete"))
+  expect_true(verify_status("abandoned"))
+  expect_true(verify_status("masked"))
+
 })

b57554d06fdbaef2b2c2bea7b767d2119b4c6df3
Danielle Navarro <d.navarro@unsw.edu.au>
2019-09-06 19:55:13 +1000

    move prompt_jobname to job_switch

--- R/job_modify.R
+++ R/job_modify.R
@@ -136,21 +136,5 @@ prompt_tag <- function(jobname) {}
 # }
 
 
-prompt_jobname <- function() {
-  opts <- job_view()$jobname
-  if(length(opts)>9) opts <- opts[1:9]
-  for(i in 1:length(opts)) {
-    cat(i, ":", opts[i], "\n")
-  }
-  cat("other: please type jobname\n")
-  jnum <- readline("which job? ")
-  if(jnum %in% as.character(1:length(opts))) {
-    jobname <- opts[as.numeric(jnum)]
-  } else {
-    jobname <- jnum
-  }
-  return(jobname)
-}
-
 
 
--- R/job_switch.R
+++ R/job_switch.R
@@ -39,4 +39,20 @@ job_switch <- function(jobname = NULL) {
 
 }
 
+prompt_jobname <- function() {
+  opts <- job_view()$jobname
+  if(length(opts)>9) opts <- opts[1:9]
+  for(i in 1:length(opts)) {
+    cat(i, ":", opts[i], "\n")
+  }
+  cat("other: please type jobname\n")
+  jnum <- readline("which job? ")
+  if(jnum %in% as.character(1:length(opts))) {
+    jobname <- opts[as.numeric(jnum)]
+  } else {
+    jobname <- jnum
+  }
+  return(jobname)
+}
+
 

8d6cb8f0c9c8dd6a1a8a77d35c8b919fb76fb95c
Danielle Navarro <d.navarro@unsw.edu.au>
2019-09-17 14:40:54 +1000

    documentation for job_openurl

--- R/job_openurl.R
+++ R/job_openurl.R
@@ -3,6 +3,17 @@
 #'
 #' @param site label denoting the site (e.g., "github")
 #' @param jobname name of the job
+#'
+#' @details The \code{job_openurl()} function opens a URL associated with a
+#' job in a browser window. The \code{site} argument is the site nickname
+#' (e.g. "github", "homepage", etc) and the \code{jobname} argument specifies
+#' the job to use.
+#'
+#' If no argument is specified \code{job_openurl()} will attempt to guess the
+#' current job by looking at any open RStudio projects. If no project is open
+#' or the RStudio API is not available it attempts to guess by looking at the
+#' working directory.
+#'
 #' @export
 job_openurl <- function(site, jobname = NULL) {
 

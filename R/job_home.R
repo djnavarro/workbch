fc6ca223bab50ed347ec66faa8c9a891b648a7b3
Danielle Navarro <d.navarro@unsw.edu.au>
2019-09-17 13:06:39 +1000

    documents job_home and adds default behaviour for jobname

--- R/job_home.R
+++ R/job_home.R
@@ -1,11 +1,29 @@
 
 #' Path to the job home
 #'
-#' @param jobname Name of job
+#' @param jobname Name of job (defaults to current job)
+#'
+#' @details The \code{job_home()} function returns the file path for `jobname`.
+#' This can be useful if you need to specify the location of files relative to
+#' a project.
+#'
+#' If no argument is specified \code{job_home()} will attempt to guess the
+#' current job by looking at any open RStudio projects. If no project is open
+#' or the RStudio API is not available it attempts to guess by looking at the
+#' working directory.
+#'
 #' @export
-job_home <- function(jobname) {
-  verify_jobname(jobname)
+job_home <- function(jobname = NULL) {
+
+  # read jobs and use the current job if the user does
+  # not specify an argument
   jobs <- job_read()
+  if(is.null(jobname)) {
+    jobname <- suppressMessages(job_getcurrent(jobs))
+  } else {
+    verify_jobname(jobname)
+  }
+
   path <- jobs[[jobname]]$path
   if(length(path) == 0) {
     stop("No path known for job '", jobname, "'", call. = FALSE)

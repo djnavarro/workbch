0c3ebe045349638e130c69fde133325c109dd602
Danielle Navarro <d.navarro@unsw.edu.au>
2019-09-17 15:02:19 +1000

    documentation for job_seek

--- R/job_seek.R
+++ R/job_seek.R
@@ -9,6 +9,34 @@
 #' @param status default status for a created job
 #' @param tags default tags for a created job
 #'
+#' @details The \code{job_seek()} searches the folders listed in \code{dirs}
+#' to find possible candidates that could be added as workbch jobs, or to
+#' recover the location of any such jobs that may have moved. When candidate
+#' folders are found, the user is presented with prompts asking if they would
+#' like to add the folder as a workbch job (or pair it with an existing job
+#' if a match is discovered).
+#'
+#' The search process uses a heuristic to discover possible jobs. A folder
+#' is considered a possible candidate if it has a \code{.Rproj} file, a
+#' \code{.workbch} file, or a git repository. If \code{nesting = FALSE} (the
+#' default), a directory that is contained within another directory that meets
+#' one of these criteria is not considered a candidate.
+#'
+#' As much as possible the \code{job_seek()} function attempts to guess values
+#' for the various parameters. For instance, if there is a git repository that
+#' has an upstream remote (on gitbub, bitbucket or gitlab), it attempts to guess
+#' an appropriate URL for the remote. Similarly, if there is a \code{.workbch}
+#' sentinel file, it uses the contents of the file to try to match the folder
+#' against a known job and takes the jobname from the sentinel file. Otherwise
+#' it guesses a default value using the folder name.
+#' For parameter values that cannot be guessed from the folder itself, default
+#' values are specified using the \code{owner}, \code{priority}, \code{status}
+#' and \code{tags} arguments.
+#'
+#' Regardless of the values guessed, the user is prompted either to confirm
+#' the guessed value (by hitting enter) or overriding the guess by supplying
+#' the value manually.
+#'
 #' @export
 job_seek <- function(dirs = getOption("workbch.search"),
                      seek = c("workbch", "git", "Rproj"),
@@ -343,223 +371,4 @@ guess_jobname <- function(path) {
   jobname <- janitor::make_clean_names(jobname)
   return(jobname)
 }
-
-
-
-# work_scan <- function(dir, owner = NULL, status = NULL, priority = NULL) {
-#
-#   # verification step for other inputs
-#   verify_status(status)
-#   verify_priority(priority)
-#
-#   # find all git repositories
-#   found_paths <- list.files(
-#     path = dir, pattern = "\\.git$", full.names = TRUE,
-#     recursive = TRUE, include.dirs = TRUE, all.files = TRUE)
-#   found_paths <- gsub("\\.git$", "", found_paths)
-#   found_paths <- normalizePath(found_paths, winslash = .Platform$file.sep)
-#
-#   # guess the job name by splitting the path to find terminal folder
-#   found_jobnames <- strsplit(found_paths, .Platform$file.sep, fixed = TRUE)
-#   found_jobnames <- purrr::map_chr(found_jobnames, ~ .x[length(.x)])
-#   found_jobnames <- janitor::make_clean_names(found_jobnames)
-#
-#   # load the existing jobs
-#   jobs <- job_read()
-#   job_names <- job_getnames(jobs)
-#   job_paths <- job_getpaths(jobs)
-#   job_paths <- suppressWarnings( # suppress b/c some jobs may have no path
-#     normalizePath(job_paths, winslash = .Platform$file.sep)
-#   )
-#
-#   # initialise output
-#   created_jobs <- tibble::tibble(jobname = character(0), path = character(0))
-#   n_known <- 0
-#   n_skipped <- 0
-#   n_created <- 0
-#
-#   # add jobs
-#   for(i in seq_along(found_paths)) {
-#
-#     # if the path is already mapped to a job, skip it
-#     if(found_paths[i] %in% job_paths) {
-#       n_known <- n_known + 1
-#       message("repo at '", found_paths[i], "' is listed... skipping")
-#
-#       # for a new path, make guesses and ask the user
-#     } else {
-#
-#       # if there is a git repository, try to find the remote
-#       found_remote <- git2r::remote_url(found_paths[i])[1] # use first
-#       site <- NA
-#       if(grepl("bitbucket|github|gitlab", found_remote)) { # if it's bb/gh/gl..
-#         site <- gsub(".*(bitbucket|github|gitlab).*", "\\1", found_remote)
-#         found_remote <- strsplit(found_remote, "[/:]")[[1]]
-#         user <- found_remote[length(found_remote)-1]
-#         repo <- found_remote[length(found_remote)]
-#         repo <- gsub("\\.git$", "/", repo)
-#         if(site == "bitbucket") prefix <- "https://bitbucket.org/"
-#         if(site == "github") prefix <- "https://github.com/"
-#         if(site == "gitlab") prefix <- "https://gitlab.com/"
-#         url_path <- paste0(prefix, user, "/", repo)
-#       }
-#
-#       # ensure the found job name doesn't already exist
-#       # ... TODO: do this better!!!
-#       while(found_jobnames[i] %in% job_names) {
-#         found_jobnames[i] <- paste0(found_jobnames[i], "X")
-#       }
-#       # construct message to the for the user
-#       message("")
-#       message("unlisted repository found:")
-#       message("    ")
-#       message("    jobname:     ", found_jobnames[i])
-#       message("    owner:       ", owner)
-#       message("    path:        ", found_paths[i])
-#       if(!is.na(site)){
-#         message("    ", site, " url:  ", url_path)
-#       }
-#       message("    status:      ", status)
-#       message("    priority:    ", priority)
-#       message("    ")
-#
-#       # make the user decide
-#       acc <- ""
-#       while(acc != "y" & acc != "n") {
-#         acc <- readline("    create job with these values? [y/n] ")
-#         acc <- tolower(acc)
-#         acc <- trimws(acc, "both")
-#       }
-#       message("")
-#
-#       # if the user says no, skip
-#       if(acc == "n") {
-#         n_skipped <- n_skipped + 1
-#         message("    ... ok, skipping")
-#       }
-#
-#       # if the user says yes, update
-#       if(acc == "y") {
-#         n_created <- n_created + 1
-#
-#         # create new job... without url
-#         if(is.na(site)) {
-#           jobs[[found_jobnames[i]]] <- new_job(
-#             jobname = found_jobnames[i],
-#             description = found_jobnames[i],
-#             owner = owner,
-#             status = status,
-#             priority = priority,
-#             path = found_paths[i]
-#           )
-#
-#           # or else create new job... with a url
-#         } else {
-#           jobs[[found_jobnames[i]]] <- new_job(
-#             jobname = found_jobnames[i],
-#             description = found_jobnames[i],
-#             owner = owner,
-#             status = status,
-#             priority = priority,
-#             path = found_paths[i],
-#             urls = new_url(site = site, link = url_path)
-#           )
-#         }
-#
-#         # write it now, in case user aborts later
-#         job_write(jobs)
-#
-#         # update output
-#         created_jobs <- dplyr::bind_rows(
-#           created_jobs,
-#           tibble::tibble(
-#             jobname = found_jobnames[i],
-#             path = found_paths[i]
-#           )
-#         )
-#
-#         # update list of known paths/jobnames
-#         job_names <- job_getnames(jobs)
-#         job_paths <- job_getpaths(jobs)
-#         job_paths <- suppressWarnings(normalizePath(job_paths))
-#
-#         message("    ... done! new job created!")
-#       }
-#
-#     }
-#
-#   }
-#
-#   # print a summary for the user
-#   message("")
-#   message(n_known, " repositories were already known")
-#   message(n_skipped, " repositories were skipped by user")
-#   message(n_created, " repositories were created")
-#   message("")
-#
-#   return(created_jobs)
-# }
-#
-#
-#
-# work_recover <- function(dirs = getOption("workbch.search")) {
-#
-#   # find the names of missing jobs
-#   missing <- job_missingsentinels()
-#
-#   # if none are missing, invisibly return NULL
-#   if(length(missing) == 0) return(invisible(NULL))
-#
-#   # add the paths and idstrings for those
-#   dat <- job_allpaths()
-#   missing <- dat[dat$jobname %in% missing,]
-#
-#   # find all sentinel files
-#   sentinels <- find_sentinels(dirs)
-#
-#   # extract informatio from all sentinel files
-#   state <- purrr::map_dfr(sentinels, function(s) {
-#     x <- readLines(s)
-#     np <- normalizePath(gsub("\\.workbch$", "", s))
-#     tibble::tibble(jobname = x[1], idstring = x[2], foundpath = np)
-#   })
-#
-#   # try to match a sentinal to each missing job
-#   missing <- dplyr::left_join(missing, state, by = c("jobname", "idstring"))
-#   missing <- missing[,c("jobname", "idstring", "path", "foundpath")]
-#
-#   # loop over missing jobs...
-#   if(interactive()) {
-#     for(i in 1:nrow(missing)) {
-#
-#       # ask the user if they want to update the path information
-#       fpstring <- ifelse(
-#         test = is.na(missing$foundpath[i]),
-#         yes = "[none found]",
-#         no = missing$foundpath[i]
-#       )
-#       cat("\n")
-#       cat("Job '", missing$jobname[i], "':\n", sep = "")
-#       cat("   Current path... ", missing$path[i], "\n")
-#       cat("   New path....... ", fpstring, "\n")
-#       cat("\n")
-#       ans <- readline("   Do you want to update/remove the path? [y/n] ")
-#
-#       # if yes, update it
-#       if(ans == "y") {
-#         job_write(
-#           update_job(
-#             jobname = missing$jobname[i],
-#             path = missing$foundpath[i]
-#           )
-#         )
-#         cat("   Job path updated\n")
-#       }
-#
-#     }
-#   }
-#
-#   # invisibly return the data frame
-#   return(invisible(missing))
-# }
 

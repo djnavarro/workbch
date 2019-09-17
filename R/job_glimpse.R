85bf31c92fe02677456e7210810f9cbc9f49765b
Danielle Navarro <d.navarro@unsw.edu.au>
2019-09-17 13:05:54 +1000

    documentation for job_glimpse

--- R/job_glimpse.R
+++ R/job_glimpse.R
@@ -3,6 +3,13 @@
 #'
 #' @param jobname Name of the job to show (defaults to the current job)
 #'
+#' @details The \code{job_glimpse()} function displays parameter values for the
+#' job specified in the \code{jobname} argument. If no argument is specified
+#' \code{job_glimpse()} will attempt to guess the "current" job by looking at
+#' any open RStudio projects. If no project is open (or the RStudio API is not
+#' available) it attempts to guess by looking at the working directory. The
+#' output is presented to the user as a message.
+#'
 #' @return Invisibly returns the job as a list
 #' @export
 job_glimpse <- function(jobname = NULL) {
@@ -17,22 +24,24 @@ job_glimpse <- function(jobname = NULL) {
   # obtain the job
   jb <- jobs[[jobname]]
 
-  cat("\n")
-  cat("  jobname:      ", jb$jobname, "\n")
-  cat("  description:  ", jb$jobname, "\n")
-  cat("  owner:        ", jb$owner, "\n")
-  cat("  priority:     ", jb$priority, "\n")
-  cat("  status:       ", jb$status, "\n")
-  cat("  path:         ", jb$path, "\n")
-  cat("  tags:         ", jb$tags, "\n")
+  msg_str <- ""
+  msg_str <- paste0(msg_str, "\n", collapse = "")
+  msg_str <- paste0(msg_str, "  jobname:      ", jb$jobname, "\n", collapse = "")
+  msg_str <- paste0(msg_str, "  description:  ", jb$jobname, "\n", collapse = "")
+  msg_str <- paste0(msg_str, "  owner:        ", jb$owner, "\n", collapse = "")
+  msg_str <- paste0(msg_str, "  priority:     ", jb$priority, "\n", collapse = "")
+  msg_str <- paste0(msg_str, "  status:       ", jb$status, "\n", collapse = "")
+  msg_str <- paste0(msg_str, "  path:         ", jb$path, "\n", collapse = "")
+  msg_str <- paste0(msg_str, "  tags:         ", jb$tags, "\n", collapse = "")
   if(nrow(jb$urls) > 0) {
     for(i in 1:nrow(jb$urls)) {
       site <- jb$urls$site[i]
-      npad <- max(14-nchar(site), 1)
+      npad <- max(13-nchar(site), 1)
       pad <- paste(rep(" ", npad), collapse = "")
-      cat("  ", site, ":", pad, jb$urls$link[i], "\n", sep = "")
+      msg_str <- paste0(msg_str, "  ", site, ":", pad, jb$urls$link[i], "\n", collapse = "")
     }
   }
 
+  message(msg_str)
   return(invisible(jb))
 }

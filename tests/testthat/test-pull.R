ad7f0511826fad06e868c1c05709263b5cf06b25
Danielle Navarro <d.navarro@unsw.edu.au>
2019-09-18 16:37:21 +1000

    add tests for pull_jobinfo

--- tests/testthat/test-pull.R
+++ tests/testthat/test-pull.R
@@ -3,43 +3,48 @@
 loc <- tempdir()
 options(workbch.home = loc)
 
-test_that("pull_jobnames / pull_jobpaths work", {
+test_that("pull functions work", {
 
-  # delete the job file if for some reason it exists
-  if(file.exists(job_file())) {
-    file.remove(job_file())
-  }
-
-  # check that job_read returns NULL for an empty file
-  jobs <- job_read()
-  expect_null(jobs)
+  jobs <- NULL
 
   # pull from empty jobnames and paths
   expect_equal(pull_jobnames(jobs), character(0))
   expect_equal(pull_jobpaths(jobs), character(0))
+  expect_equal(pull_jobinfo(jobs), tibble::tibble())
 
-  # create a list of jobs directly from the constructor
-  jobs <- suppressWarnings(list(toxic = new_job(
-    jobname = "toxic", description = "a song", owner = "britney"
-  )))
-  job_write(jobs)
+  jobs <- list()
+  jobs[["toxic"]] <- workbch:::new_job(
+    jobname = "toxic",
+    description = "a song",
+    owner = "britney",
+    sentinel = FALSE)
 
   # check job names and paths: named character vectors
   expect_equal(pull_jobnames(jobs), c(toxic = "toxic"))
   expect_equal(pull_jobpaths(jobs), c(toxic = NA_character_))
+  expect_named(pull_jobinfo(jobs), c("jobname", "path", "idstring"))
+  expect_equal(nrow(pull_jobinfo(jobs)), 0)
 
   # add a second job that does have a path
-  jobs[["hitmebaby"]] <- suppressWarnings(new_job(
+  jobs[["hitmebaby"]] <- new_job(
     jobname = "hitmebaby",
     description = "another song",
     owner = "britney",
+    sentinel = FALSE,
     path = loc
-  ))
-  job_write(jobs)
+  )
 
   # check job names and paths: named character vectors
   expect_equal(pull_jobnames(jobs), c(toxic = "toxic", hitmebaby = "hitmebaby"))
   expect_equal(pull_jobpaths(jobs), c(toxic = NA_character_, hitmebaby = loc))
+  expect_named(pull_jobinfo(jobs), c("jobname", "path", "idstring"))
+  expect_equal(nrow(pull_jobinfo(jobs)), 1)
+  expect_equal(pull_jobinfo(jobs)$jobname, "hitmebaby")
+  expect_equal(pull_jobinfo(jobs)$path, loc)
+  expect_equal(pull_jobinfo(jobs)$idstring, jobs[["hitmebaby"]]$idstring)
+
 
 })
+
+
 
